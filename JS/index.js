2d3988dfb7f9d34241e7e097c0cb9e96d8ef6629
KuChauWe <gomez.ocampo.hugo.1@gmail.com>
2021-10-02 00:39:59 -0500

    Corrección de nombres

--- JS/index.js
+++ JS/index.js
@@ -1,7 +1,7 @@
 function reedireccionar(){
     if (document.getElementById("boton 1").checked){
-        window.location = 'indexVerdadero.html';
-        window.location.assign('indexVerdadero.html');
+        window.location = 'cesar.html';
+        window.location.assign('cesar.html');
     }
     else if (document.getElementById("boton 2").checked){
         window.location = 'viggenere.html';
